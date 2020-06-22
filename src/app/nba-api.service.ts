d956ab3b14e2f0b470d410e8062e9ad8a22b178f
szareian <szareian@sfu.ca>
2020-06-22 10:52:27 -0700

    Fixed the empty search term bug

--- src/app/nba-api.service.ts
+++ src/app/nba-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Player } from './player';
 
@@ -14,7 +14,6 @@ export class NbaApiService {
     API_KEY = '';
 
     private playersUrl: string = 'https://www.balldontlie.io/api/v1/players';
-    // private all_players_url: string = 'https://www.balldontlie.io/api/v1/players?search=anthony+davis';
 
     public getAllPlayers(): Observable<Player[]>{ 
         return this.http.get<Player[]>(this.playersUrl)
@@ -24,15 +23,16 @@ export class NbaApiService {
     };
 
     public searchPlayers(term: string): Observable<Player[]>{
+        if (!term.trim()) {
+            // if not search term, return empty Player array.
+            return of([]);
+        }
         term = term.trim();
 
         // Add safe, URL encoded search parameter if there is a search term
         const options = term ?
             { params: new HttpParams().set('search', term) } : {};
 
-        if (!term){ // quick fix for when the search box is clear
-            return
-        }
         return this.http.get<Player[]>(this.playersUrl, options)
             .pipe(map((res: any) => res.data));
     };
