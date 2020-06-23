632ab4c32de71c7439de3e75bc979337005601b0
szareian <szareian@sfu.ca>
2020-06-22 19:33:33 -0700

    Removed redundant class properties

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -9,15 +9,6 @@ import { Game } from '../../game';
   templateUrl: "dashboard.component.html"
 })
 export class DashboardComponent implements OnInit {
-  public canvas : any;
-  public ctx;
-  public datasets: any;
-  public data: any;
-  public myChartData;
-  public clicked: boolean = true;
-  public clicked1: boolean = false;
-  public clicked2: boolean = false;
-
   public players: Player[];
   public Games: Game[];
 
