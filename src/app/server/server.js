b6c2f85f84e391ddd76ec10e40084c01ba38f68f
szareian <szareian@sfu.ca>
2020-06-29 11:46:11 -0700

    Added Twitter config keys to Heroku

--- src/app/server/server.js
+++ src/app/server/server.js
@@ -28,7 +28,12 @@ var allowCrossDomain = function (req, res, next) {
 };
 app.use(allowCrossDomain);
 
-const client = new Twitter(key);
+const client = new Twitter({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret
+});
 
 app.get('/home_timeline', (req, res) => {
     const params = { tweet_mode: 'extended', count: 10 };
