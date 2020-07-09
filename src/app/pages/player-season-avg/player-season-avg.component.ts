215384663a48dc737bf5507f0ae5a45f51edd4ed
szareian <szareian@sfu.ca>
2020-07-09 14:00:01 -0700

    Added stats for each player's season average with some styling/markup

--- src/app/pages/player-season-avg/player-season-avg.component.ts
+++ src/app/pages/player-season-avg/player-season-avg.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 import { NbaApiService } from "../../nba-api.service";
 import { SeasonAvg } from '../../season-average';
 import { Player } from '../../player';
@@ -8,7 +8,7 @@ import { Player } from '../../player';
   templateUrl: './player-season-avg.component.html',
   styleUrls: ['./player-season-avg.component.scss']
 })
-export class PlayerSeasonAvgComponent implements OnInit {
+export class PlayerSeasonAvgComponent implements OnChanges {
   public playerAvg: SeasonAvg[];
   @Input() currPlayer: Player; 
 
@@ -20,14 +20,12 @@ export class PlayerSeasonAvgComponent implements OnInit {
       .subscribe(
         (res) => {
         this.playerAvg = res
-          console.log(this.playerAvg)
+        // console.log(this.playerAvg)
         });
   }
-  
-  ngOnInit(): void {
-    
-    // console.log(this.postSeasonAvg(this.currPlayer.id, 2018));
-    this.postSeasonAvg(this.currPlayer.id,2018);
-  }
 
+  ngOnChanges(){
+    this.postSeasonAvg(this.currPlayer.id, 2018);
+    
+  }
 }
