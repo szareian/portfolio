95765a7927090d2764127845ad634040a52840aa
szareian <szareian@sfu.ca>
2020-07-24 14:01:51 -0700

    Re-implemented the pop-up page using material's dialog

--- src/app/pages/player-search/player-search.component.ts
+++ src/app/pages/player-search/player-search.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
-import { trigger, transition, animate, style } from '@angular/animations';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
+import { PlayerSeasonAvgComponent } from "../player-season-avg/player-season-avg.component";
 import { NbaApiService } from "../../nba-api.service";
 import { Player } from '../../player';
 
@@ -10,30 +11,17 @@ import { Player } from '../../player';
   selector: 'app-player-search',
   templateUrl: './player-search.component.html',
   styleUrls: ['./player-search.component.scss'],
-  animations: [
-    trigger('insertRemoveStats', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('100ms', style({ opacity: 1 })),
-      ]),
-      transition(':leave', [
-        animate('100ms', style({ opacity: 0 }))
-      ])
-    ]),
-  ]
 })
 export class PlayerSearchComponent implements OnInit {
 
-  public isHidden = true;
   public players$: Observable<Player[]>;
-  public CurrPlayer: Player;
-  public headShot: String;
   public defaultHeadshot = '../../../assets/img/default-avatar.png';
   private searchTerms = new Subject<string>();
   private headShotBaseUrl = 'https://nba-players.herokuapp.com/players';
 
-
-  constructor(private nbaapiservice: NbaApiService) { }
+  constructor(
+    private nbaapiservice: NbaApiService,
+    public dialog: MatDialog) { }
 
   // Push a search term into the observable stream.
   search(term: string): void {
@@ -45,21 +33,25 @@ export class PlayerSearchComponent implements OnInit {
     return results
   }
 
-  onClick(p: Player){
-    this.CurrPlayer = p;
-
-    this.toggle();
-  }
-
-  toggle() {
-    this.isHidden = !this.isHidden;
-
-  }
- 
   trackByMethod(index: number, p: Player): number {
     return p.id;
   }
-  
+
+  openDialog(p: Player) {
+    const dialogConfig = new MatDialogConfig()
+
+    dialogConfig.data = {
+      CurrPlayer: p,
+      headShot: this.postHeadshot(p)
+    }
+    
+    const dialogRef = this.dialog.open(PlayerSeasonAvgComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
   ngOnInit(): void {
     this.players$ = this.searchTerms.pipe(
       // wait 300ms after each keystroke before considering the term
@@ -70,7 +62,7 @@ export class PlayerSearchComponent implements OnInit {
 
       // switch to new search observable each time the term changes
       switchMap((term: string) => this.nbaapiservice.searchPlayers(term)),
-    );      
+    );
   }
-  
+
 }
