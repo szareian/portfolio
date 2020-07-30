70553343e9c2e02c84b172af7ced277d3a4527b3
szareian <szareian@sfu.ca>
2020-07-30 11:48:41 -0700

    Implemented the input areas and button for specifying the NBA team and season

--- src/app/pages/team-schedule/team-schedule.component.ts
+++ src/app/pages/team-schedule/team-schedule.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, Subject, merge } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
+
 
 import { NbaApiService } from "../../nba-api.service";
-
-import { Observable } from 'rxjs';
-
 
 @Component({
     selector: "app-team-schedule",
@@ -11,12 +12,31 @@ import { Observable } from 'rxjs';
     styleUrls: ['team-schedule.component.scss']
 })
 export class TeamScheduleComponent implements OnInit {
+    
+    @ViewChild('instance', { static: true }) instance: NgbTypeahead;
+    public focus$ = new Subject<string>();
+    public click$ = new Subject<string>();
     public schedule: Observable<any>;
-    public NBATeams: string[] = ['hawks', 'celtics', 'nets', 'hornets', 'bulls', 'cavaliers', 'mavericks', 'nuggets', 'pistons', 'warriors',
-        'rockets', 'pacers', 'clippers', 'lakers', 'grizzlies', 'heat', 'bucks', 'timberwolves', 'pelicans', 'knicks', 'thunder', 'magic', 'sixers',
-        'suns', 'blazers', 'kings', 'spurs', 'raptors', 'jazz', 'wizards'];
+    public teams: string[] = ['hawks', 'celtics', 'nets', 'hornets', 'bulls', 'cavaliers', 'mavericks', 'nuggets', 'pistons', 'warriors',
+    'rockets', 'pacers', 'clippers', 'lakers', 'grizzlies', 'heat', 'bucks', 'timberwolves', 'pelicans', 'knicks', 'thunder', 'magic', 'sixers',
+    'suns', 'blazers', 'kings', 'spurs', 'raptors', 'jazz', 'wizards'];
+    public teamsSorted = this.teams.sort();
+    public selectedTeam: any;
 
     constructor(private nbaapiservice: NbaApiService) { }
+
+
+
+    search = (text$: Observable<string>) => {
+        const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+        const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+        const inputFocus$ = this.focus$;
+
+        return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+            map(term => (term === '' ? this.teamsSorted
+                : this.teamsSorted.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 5))
+        );
+    }
 
     getGameDate(d : string, time: string){
         let game_date = d.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
@@ -30,7 +50,7 @@ export class TeamScheduleComponent implements OnInit {
         }
         return full_date
     }
-    
+
     postTeamSchedule(team: string, year: number) {
         this.nbaapiservice.getTeamSchedule(team, year).subscribe(
             (res) => {
@@ -40,7 +60,7 @@ export class TeamScheduleComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.postTeamSchedule('clippers', 2019);
+        // this.postTeamSchedule('raptors', 2018);
     }
 }
 
